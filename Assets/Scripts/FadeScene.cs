34c2ae6b1126982b4ddfed1d9c104c1fa869501b
Eric Dong <ericdong@usc.edu>
2015-10-01 02:05:50 -0700

    First attempt at intro camera sequence

---
+++ Assets/Scripts/FadeScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Image = UnityEngine.UI.Image;
+
+public class FadeScene : MonoBehaviour {
+
+    private Image myImage;
+    private Color myColor;
+    private bool fadingIn;
+
+	void Start () {
+        fadingIn = false;
+        myImage = this.GetComponent<Image>();
+        myColor = myImage.color;
+	}
+	
+	void Update () {
+        if (fadingIn) {
+            myColor.a -= 0.3f * Time.deltaTime;
+            if (myColor.a <= 0) {
+                fadingIn = false;
+                myColor.a = 0;
+            }
+
+            myImage.color = myColor;
+        }
+	}
+
+    public void fadeIn() {
+        fadingIn = true;
+    }
+}
