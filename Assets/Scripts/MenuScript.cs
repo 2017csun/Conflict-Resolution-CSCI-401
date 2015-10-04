f1fb68de9c1b182b566ceaed1b2c9283df18ccfe
Jiwoo Kim <jiwookim@usc.edu>
2015-10-03 23:40:34 -0700

    Main Menu Progress

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class MenuScript : MonoBehaviour {
+	
+	public Button exitButton;
+	public Button helpButton;
+	public Button button1Button;
+	public Button button2Button;
+	// Use this for initialization
+	void Start () {
+		button1Button = button1Button.GetComponent<Button> ();
+		button2Button = button2Button.GetComponent<Button> ();
+		helpButton = helpButton.GetComponent<Button> ();
+		exitButton = exitButton.GetComponent<Button> ();
+	}
+	
+	public void ExitPressed() {
+		Application.Quit ();
+	}
+	
+	public void HelpPressed() {
+		/*
+		button1Button.enabled = false;
+		button2Button.enabled = false;
+		helpButton.enabled = false;
+		*/
+	}
+	
+	public void Button1Pressed() {
+		/*
+		exitButton.enabled = false;
+		button2Button.enabled = false;
+		helpButton.enabled = false;
+		*/
+	}
+	
+	public void Button2Pressed() {
+		button2Button.GetComponentInChildren<Text> ().text = "SPACESHIP";
+		/*
+		button1Button.enabled = false;
+		exitButton.enabled = false;
+		helpButton.enabled = false;
+		*/
+	}
+}
