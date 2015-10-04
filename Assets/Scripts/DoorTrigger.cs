c8d4f7e5f0e2692a4d3c74dfd86a921bfddb7637
Eric Dong <ericdong@usc.edu>
2015-10-04 01:03:33 -0700

    Some networking code

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrigger : MonoBehaviour {
+
+    public GameObject[] doors;
+
+    void OnTriggerEnter (Collider col) {
+        for (int i = 0; i < doors.Length; ++i) {
+            doors[i].GetComponent<Door>().OpenDoor();
+        }
+    }
+}
