d71d66bd4da5051d3aaa4620fa1a61f7860010cb
Eric Dong <ericdong@usc.edu>
2015-10-08 19:59:30 -0700

    networking

--- Assets/Scripts/ConflictNetworkManager.cs
+++ Assets/Scripts/ConflictNetworkManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
 using UnityEngine.Networking.Types;
@@ -12,6 +13,7 @@ public class ConflictNetworkManager : NetworkManager {
 	private HostData[] hostData;
 	private bool isRefreshingHosts = false;
 
+	private string gameCode;
 	private string serverToConnect;
     private NetworkClient myClient;
     private NetworkMatch networkMatch;
@@ -19,6 +21,7 @@ public class ConflictNetworkManager : NetworkManager {
 	void Start () {
         networkMatch = this.gameObject.AddComponent<NetworkMatch>();
         networkMatch.SetProgramAppID((AppID)379051);
+		gameCode = "";
 	}
 	
 	void OnMasterServerEvent (MasterServerEvent mse) {
@@ -64,26 +67,26 @@ public class ConflictNetworkManager : NetworkManager {
 	public void startServer () {
         myClient = this.StartHost();
 
-        //string gameCode = "";
-        //int codeLength = 6;
-        //string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		int codeLength = 6;
+		string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		
+		//  Generate random string
+		for (int i = 0; i < codeLength; ++i) {
+			char randomChar = chars[Random.Range(0, chars.Length)];
+			gameCode += randomChar;
+		}
+		
+		gameCode = "ericdongisawesome";
+		Debug.Log("Creating match under name: " + gameCode);
 
-        ////  Generate random string
-        //for (int i = 0; i < codeLength; ++i) {
-        //    char randomChar = chars[Random.Range(0, chars.Length)];
-        //    gameCode += randomChar;
-        //}
+		//	Create the matchmaker request
+        CreateMatchRequest create = new CreateMatchRequest();
+        create.name = gameCode;
+        create.size = 2;
+        create.advertise = true;
+        create.password = "";
 
-        //gameCode = "eric_dong_is_awesome";
-        //Debug.Log("Registering under code: " + gameCode);
-        //CreateMatchRequest create = new CreateMatchRequest();
-        //create.name = gameCode;
-        //create.size = 2;
-        //create.advertise = true;
-        //create.password = "";
-
-        //networkMatch.CreateMatch(create, OnMatchCreate);
-        //MasterServer.RegisterHost(gameCode, "Conflict Resolution", "Fun times at USC");
+        networkMatch.CreateMatch(create, OnMatchCreate);        
 	}
 
     public override void OnMatchCreate (CreateMatchResponse matchResponse) {
@@ -100,13 +103,44 @@ public class ConflictNetworkManager : NetworkManager {
         }
     }
 
+	public override void OnStartHost () {
+		base.OnStartHost();
+
+		Debug.Log("Host started!");
+	}
+
 	//--------------------------------------------------
 	//	Client Code
 	//--------------------------------------------------
 
 	public void connectToServer (string gameTypeName) {
-		MasterServer.RequestHostList(gameTypeName);
-		serverToConnect = gameTypeName;
-		isRefreshingHosts = true;
+		networkMatch.ListMatches(0, 20, gameTypeName, OnMatchList);
+	}
+
+	public override void OnMatchList (ListMatchResponse matchListResponse) {
+		List<MatchDesc> matches = matchListResponse.matches;
+
+		if (matches.Count > 1) {
+			Debug.LogError("THERE ARE MULTIPLE MATCHES IDK WHAT TO DOOOOO");
+		}
+		else if (matches.Count == 1) {
+			networkMatch.JoinMatch(matches[0].networkId, "", OnMatchJoined);
+		}
+		else {
+			Debug.LogError("MATCH NOT FOUND!!");
+		}
+	}
+
+	public void OnMatchJoined (JoinMatchResponse join) {
+		Debug.Log("Match joined successfully!");
+		if (join.success) {
+			NetworkClient myClient = new NetworkClient();
+			myClient.RegisterHandler(MsgType.Connect, OnConnected);
+			myClient.Connect(new MatchInfo(join));
+		}
+	}
+
+	public void OnConnected (NetworkMessage msg) {
+		Debug.Log("Connected!");
 	}
 }
