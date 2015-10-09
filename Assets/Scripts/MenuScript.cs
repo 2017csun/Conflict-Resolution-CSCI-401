d1248e4ad91543cc13ff81924cd61ec4c479b0c3
Eric Dong <ericdong@usc.edu>
2015-10-08 19:11:39 -0700

    stuff before i can do more stuff

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -30,7 +30,6 @@ public class MenuScript : MonoBehaviour {
 	}
 	
 	public void Button1Pressed() {
-        Application.LoadLevel(2);
 		/*
 		exitButton.enabled = false;
 		button2Button.enabled = false;
