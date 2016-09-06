09c5a900cfa32967c897410cc544e331039d4b17
Carrie Sun <carriesu@usc.edu>
2016-09-05 23:59:50 -0700

    prep for webGL and some obsolete updating

--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -11,7 +11,8 @@ public class ForcedReset : MonoBehaviour
         if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
         {
             //... reload the scene
-            Application.LoadLevelAsync(Application.loadedLevelName);
+            //Application.LoadLevelAsync(Application.loadedLevelName);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
